4f9f3500aedf3fa263f10a25aa4ac790bdce3337
MK16020 <61877101+MK16020@users.noreply.github.com>
2023-01-16 20:49:59 +0300

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class homeWork extends StatelessWidget {
+  const homeWork({super.key});
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
